c02d12f681e4100783c9b829ebbf56c12b286f3d
Júlio Sanches <juliosanches@gmail.com>
2024-02-01 17:25:13 -0300

    Seção 7: Mockando um LocalDateTime

--- src/test/java/br/com/erudio/mockito/services/OrderServiceTest.java
+++ src/test/java/br/com/erudio/mockito/services/OrderServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,6 +14,7 @@ class OrderServiceTest {
 
     private final OrderService service = new OrderService();
     private final UUID defaultUuid = UUID.fromString("df39b0be-d002-485a-bfc6-616be887da62");
+    private final LocalDateTime defaultLocalDateTime = LocalDateTime.of(2024, 2,1,17,20);
 
     // test[System Under Test]_[Condition or State Change]_[Expected Result]
     @Test
@@ -27,10 +29,19 @@ class OrderServiceTest {
             assertEquals(defaultUuid.toString(), result.getId());
 
         }
+    }
 
+    @Test
+    @DisplayName("Should Include CreateTime When Create A New Order")
+    void testShouldIncludeCreateTime_When_CreateANewOrder() {
+        // Given / Arrange
+        try (MockedStatic<LocalDateTime> mockedLocalDateTime = mockStatic(LocalDateTime.class)) {
+            mockedLocalDateTime.when(LocalDateTime::now).thenReturn(defaultLocalDateTime);
+            // When / Act
+            Order result = service.createOrder("Positivo Vaio", 2L, null);
+            // Then / Assert
+            assertEquals(defaultLocalDateTime, result.getCreationDate());
 
-
-
-
+        }
     }
 }
